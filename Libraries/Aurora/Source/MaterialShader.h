5813560d059438ccb7e0262430f8606e64c70acc
adro79 <57686179+adro79@users.noreply.github.com>
2023-09-17 16:09:29 +0200

    Update MaterialShader.h

--- Libraries/Aurora/Source/MaterialShader.h
+++ Libraries/Aurora/Source/MaterialShader.h
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #pragma once
+#pragma GCC diagnostic ignored "-Wreorder-ctor"
 
 #include "Aurora/Foundation/Log.h"
 #include "Properties.h"
 #include "UniformBuffer.h"
+#include <list>
 
 BEGIN_AURORA
 
